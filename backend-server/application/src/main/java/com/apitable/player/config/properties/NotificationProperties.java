b2878f0a9814474df1cfcdce784d0a598d10cb3e
Zoe <zhengxu@apitable.com>
2025-01-09 11:11:19 +0800

    fix: checkstyle (#11415)
    @coderabbitai: ignore
    
    Co-authored-by: Zoe <13400804+zoe-icu@users.noreply.github.com>

--- backend-server/application/src/main/java/com/apitable/player/config/properties/NotificationProperties.java
+++ backend-server/application/src/main/java/com/apitable/player/config/properties/NotificationProperties.java
@@ -18,6 +18,9 @@ package com.apitable.player.config.properties;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+/**
+ * notification properties.
+ */
 @Data
 @ConfigurationProperties(prefix = "notification")
 public class NotificationProperties {
